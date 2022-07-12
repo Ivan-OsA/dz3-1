2c6c1c57c9f386654d94128b0c78755a40bec998
Иван Осокин <osokin_ivan1992@mail.ru>
2022-07-12 21:06:18 +0300

    new ignore

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,8 @@
-public class BonusMilesService{
-    public int calculate(int cost){
-      int price = cost;
-      int miles = (cost/20);
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = cost;
+        int miles = (cost / 20);
+
         return miles;
     }
 
